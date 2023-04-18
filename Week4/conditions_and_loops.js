0131c4b63abab7069db8088d5df48ac85ce20b0f
Tetiana <tetiana.kravchenko@powercoders.org>
2023-04-18 16:08:16 +0200

    Solved task Odd/Even Reporter 2.0 using loop for.

--- Week4/conditions_and_loops.js
+++ Week4/conditions_and_loops.js
@@ -154,4 +154,17 @@ if((num < 0) || (num > 20)){
 }
 
 /*Task 8
+Odd / Even Reporter 2.0
+Write a for loop that will iterate from 0 to 20.
+For each iteration, it will check if the current number is even or
+odd.
+Report the result to the screen (e.g. "2 is even").*/
+
+for(let num = 0; num < 21; num++){
+    if(num%2 === 0){
+        console.log(`${num} is even`);
+    }else{
+        console.log(`${num} is odd`);
+    }
+}
 
