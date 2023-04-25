bc9c31f6b51915e1943febac11da5bcf01fc8471
Tetiana <tetiana.kravchenko@powercoders.org>
2023-04-25 10:49:28 +0200

    Add recap of functions to classrom project.

---
+++ Week5/Objects/script.js
@@ -0,0 +1,66 @@
+//Task1 Claaasrom project recap
+/*
+// Arrow function
+myArr.forEach((person)=>{
+    //code to execute
+});
+
+//Function declaration: you can call before, after, inside function declaration
+function handleClickLuzern(){
+    //code
+}
+
+//Assign the function to a variable. Can't be called after
+const handleClickLuzern2 = function(){
+    //code
+}
+
+//Arrow function, most used. Best practice: use for anonyms function
+const handleClickLuzer3=()=>{
+
+}
+
+document.getElementById('content').innerHTML+=`
+    <div>
+`;
+
+//code refectoring
+
+function handleClick(arr){
+    document.getElementById(`content`).innerHTML = "";
+    arr.forEach((person) => {
+        document.getElementById('content').innerHTML+=`
+        <div>
+        </div>
+`;
+    })
+};
+
+
+//add event Listeners for click and mouseover
+const filterButtons = document.querySelectorAll("button");
+filterButtons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        //console.log(button.getAttribute("id"));
+        switch(button.getAttribute("id")){
+            case "btn-Luzerner":
+                handleClick(luzerner);
+                break;
+            default:
+                handleClick(myArr);
+        }
+        handleClick();
+    })
+});
+
+//add event Listeners for mouseover
+const images = querySelectorAll("img");
+images.forEach(image => {
+    image.addEventListener("mouseover", () =>{
+        image.classList.add("imgEffect")
+    });
+    
+});
+*/
+
+//Task 2
