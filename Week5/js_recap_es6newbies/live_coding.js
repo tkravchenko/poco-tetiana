518be8f7edfdfc5b35f6e96fbab999451b9ece33
Tetiana <tetiana.kravchenko@powercoders.org>
2023-04-27 10:13:39 +0200

    Add class and sub-class.

--- Week5/js_recap_es6newbies/live_coding.js
+++ Week5/js_recap_es6newbies/live_coding.js
@@ -15,7 +15,7 @@ const car = {
 car.checkTankAmount();
 
 //object constructor
-function Car(brand, model, color, fuel, doors, year){
+function Cars(brand, model, color, fuel, doors, year){
     this.brand = brand;
     this.model = model;
     this.color = color;
@@ -23,11 +23,37 @@ function Car(brand, model, color, fuel, doors, year){
     this.doors = doors;
     this.year = year;
     this.checkTankAmount = function(){
-        console.log(`Hey, you need to drive to the next gas station with your ${htis.brand} ${this.model}..`);
+        console.log(`Hey, you need to drive to the next gas station with your ${htis.brand} ${this.model}.`);
 
     }
 };
 
 //create instance
-let susanneCar = new Car("Mazda", "3", "grey", "Diesel", 4, 2005);
+let susanneCar = new Cars("Mazda", "3", "grey", "Diesel", 4, 2005);
 console.log(susanneCar);
+
+//class vehicle - car, motorcycle, truck, plane, ship, bicycle
+class Vehicle {
+    constructor(brand, model, color, year){
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+}
+
+class Car extends Vehicle {//is a subclass of Vehicle, iheriting principles
+    //always has a constructor
+    constructor(brand, model, color, fuel, doors, year){
+        super(brand, model, color, year);//to add parameters
+        this.fuel = fuel;
+        this.doors = doors;
+    }
+    checkTankAmount = function(){
+        console.log(`Hey, you need to drive to the next gas station with your ${htis.brand} ${this.model}.`);
+
+    }
+};
+
+let mustafaCar = new Car("Ford", "Fiesta", "white", "Petrol", 2, 1990);
+console.log(mustafaCar);
