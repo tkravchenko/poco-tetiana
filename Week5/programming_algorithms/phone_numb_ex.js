3472e71767377eb5ae3594875276ce1d7beec28f
Tetiana <tetiana.kravchenko@powercoders.org>
2023-04-28 12:27:24 +0200

    Solved phone_validation task using functions and conditions.

---
+++ Week5/programming_algorithms/phone_numb_ex.js
@@ -0,0 +1,38 @@
+let phoneNumber = "";
+
+do {
+    //clear the phone number
+    if(phoneNumber){
+        phoneNumber = "";
+    };
+    phoneNumber = prompt("Please enter your phonr number, e.g. 079 879 9236.");
+} while (!(isValidNumber(phoneNumber)));
+
+//check if the number is valid
+function isValidNumber(phoneNumber) {
+    let isValid = false;
+    //remove the white spaces and special characters
+    phoneNumber = phoneNumber.replace(/[\-\s\(\)\.]/g, '');
+    if (!(isNaN(phoneNumber))){
+        if (phoneNumber.length === 10){
+            isValid = true;
+            alert("Your number has been successfully stored!");
+        }else if (phoneNumber === "") {
+            alert("You didn't enter the phone number.")
+        }else{
+            alert("Your number is invalid");
+        }
+    }else{
+        alert("Your number is invalid");
+    }
+    return isValid;
+};
+
+
+
+///
+let phoneNumber = "";
+
+phoneNumber = prompt("Please enter your phonr number, e.g. 079 879 9236.");
+phoneNumber = phoneNumber.replace(" ", "");
+console.log(phoneNumber);
