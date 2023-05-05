91b9f9695e5acad81379109e579c3a059591d9e1
Tetiana <tetiana.kravchenko@powercoders.org>
2023-05-05 13:49:17 +0200

    Updated using local storage.

--- Week6/ajax_jason/image_gallery/script.js
+++ Week6/ajax_jason/image_gallery/script.js
@@ -1,47 +1,31 @@
-/*load photos via fetch and display 10 of them as images with title (as caption and alt)
-
-*fetch the data and turning json to object list
-*loop through the array 10 times
-*create Element img tag to display the image
-*create 2nd element fot title - eiter h-tag or figcaption
-*we use figure as wrapper element for img and figcaption
-*we append the figure elements to the DOM, to the main
-*we add figures in unorderd list instead
-*/
-
-
-fetch('https://jsonplaceholder.typicode.com/photos?albumID=1')
-.then(response => response.json())
-.then(json => displayData(json))
-
+/** load photos via fetch and display 10 of them as images with title (as caption and as alt)
+ *
+ * fetch the data and turning json to object list
+ * loop through the array 10 times
+ * create element img tag to display the image
+ * create 2nd element for title - either h-tag or figcaption
+ * we use figure as wrapper element for img and figcaption
+ * we append the figure elements to the DOM, to the main
+ * we add figures in unordered list instead
+ */
+localStorage.setItem("key-ömer", "value-ömer");
 const main = document.querySelector("main");
-/*
-function displayData(data){
-    //data = data.slice(0,10);
-    data.slice(0,10).forEach(photoObj => {
-        let figure = document.createElement("figure");
 
-        console.log(photoObj);
-        let img = document.createElement("img");
-        img.src = photoObj.url;
-        img.alt = photoObj.title;
-        figure.appendChild(img);
-        //main.appendChild(img);
-
-        let figcaption = document.createElement("figcaption");
-        figcaption.textContent = photoObj.title;
-
-        figure.appendChild(figcaption);
-        main.appendChild(figure);
-    });
+if (localStorage.getItem("burak")) {
+    displayData();
+} else {
+    fetch("https://jsonplaceholder.typicode.com/photos?albumId=1")
+        .then((response) => response.json())
+        .then((data) => localStorage.setItem("burak", JSON.stringify(data)))
+        .then(() => displayData());
 }
-*/
 
-function displayData(data){
-    //data = data.slice(0,10);
-    const ul =document.createElement("ul");
+function displayData() {
+    let data = JSON.parse(localStorage.getItem("burak"));
+    const ul = document.createElement("ul");
     main.appendChild(ul);
-    data.slice(0,10)forEach(photoObj => {
+
+    data.slice(0, 10).forEach((photoObj) => {
         let li = document.createElement("li");
         ul.appendChild(li);
 
@@ -52,12 +36,9 @@ function displayData(data){
         img.src = photoObj.url;
         img.alt = photoObj.title;
         figure.appendChild(img);
-        //main.appendChild(img);
 
         let figcaption = document.createElement("figcaption");
         figcaption.textContent = photoObj.title;
-
         figure.appendChild(figcaption);
-        main.appendChild(figure);
     });
 }
