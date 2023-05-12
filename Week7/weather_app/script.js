d80ef79630fd207e281bdb921f189700b0dd11b5
Tetiana <tetiana.kravchenko@powercoders.org>
2023-05-12 09:14:05 +0200

    Create files for weather app.

---
+++ Week7/weather_app/script.js
@@ -0,0 +1,10 @@
+/*
+Create a mini project with Weather Forecast API
+Create a minimal weather interface in HTML/CSS & Javascript
+Get the weather datas that interest you the most (Eg.
+Temperature, Humidity, Wind...)
+Feel free to be creative (Eg. Change background color or image
+depending current datas from API)
+Link to the API
+*/
+
